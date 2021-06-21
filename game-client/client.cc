fae92504549395db9b7745383a542da0e5d44425
TValentine <tony.valentine.19@um.edu.mt>
2021-06-21 09:15:58 +0200

    Improved Client Side command Parsing

--- game-client/client.cc
+++ game-client/client.cc
@@ -9,6 +9,7 @@
 #include <poll.h>
 #include <regex>
 #include <sstream>
+#include <string>
 #include <sys/socket.h>
 #include <thread>
 #include <unistd.h> // Linux fds
@@ -20,7 +21,6 @@ int establish_connection(char *hostname, char *port) {
   memset(&hints, 0, sizeof hints);
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
-
   int rv;
 
   // Getting a list of sockets we can connect to
@@ -50,35 +50,147 @@ int establish_connection(char *hostname, char *port) {
 }
 
 tmessage parse_message(string str) {
-  // Determining if its a command
+  tmessage msg;
+  // Determining if command
   if (str.at(0) == '!') {
-    istringstream ss(str);
+    stringstream ss(str);
     string command;
     ss >> command; // Getting the first word
-    tmessage msg;
-    strcpy(msg.buffer, str.c_str());
     if (regex_match(command, CMD_LEADERBOARD)) {
-      msg.message_type = LEADERBOARD;
+      msg.message_type = (tmessage_t)htonl((int32_t)LEADERBOARD);
+      if (regex_match(str, CMD_LEADERBOARD_FULL)) {
+        ss >> msg.arg1; // Setting arg1 to the gamemode number
+        msg.arg1 = htonl(msg.arg1);
+      } else {
+        throw invalid_argument(
+            "Unable to parse: \'" + str +
+            "\', the correct sytnax is \'!leaderboard [1-2]\'");
+      }
     } else if (regex_match(command, CMD_PLAYERS)) {
-      msg.message_type = PLAYER;
+      msg.message_type = (tmessage_t)htonl((int32_t)PLAYERS);
+      if (ss >> command) {
+        throw invalid_argument("Unable to parse: \'" + str +
+                               "\', the correct sytnax is \'!players\'");
+      }
     } else if (regex_match(command, CMD_PLAYERSTATS)) {
-      msg.message_type = PLAYERSTATS;
+      msg.message_type = (tmessage_t)htonl((int32_t)PLAYERSTATS);
+      if (regex_match(str, CMD_PLAYERSTATS_FULL)) {
+        ss >> command;
+        strcpy(msg.buffer, command.c_str());
+      } else {
+        throw invalid_argument(
+            "Unable to parse: \'" + str +
+            "\', the correct sytnax is \'!playerstats <playername>\'. The "
+            "player name must not exceed 10 characters");
+      }
     } else if (regex_match(command, CMD_NICKNAME)) {
-      msg.message_type = NICKNAME;
+      msg.message_type = (tmessage_t)htonl((int32_t)NICKNAME);
+      if (regex_match(str, CMD_NICKNAME_FULL)) {
+        ss >> command;
+        strcpy(msg.buffer, command.c_str());
+      } else {
+        throw invalid_argument(
+            "Unable to parse: \'" + str +
+            "\', the correct sytnax is \'!nickname <playername>\'. The "
+            "player name must not exceed 10 alphanumeric characters");
+      }
     } else if (regex_match(command, CMD_BATTLE)) {
-      msg.message_type = BATTLE;
+      msg.message_type = (tmessage_t)htonl((int32_t)BATTLE);
+      if (regex_match(str, CMD_BATTLE_FULL)) {
+        int mode;
+        ss >> mode;
+        msg.arg1 = htonl(mode); // Setting the gamemode
+        int arg_count =
+            count(str.begin(), str.end(), '='); // only arguments have an =
+        switch (mode) {
+        case 0: {
+          if (arg_count > 0) {
+            throw invalid_argument(
+                "Rising Tide gamemode does not take arguments");
+          } else {
+            getline(ss, str); // Getting playernames
+            strcpy(msg.buffer, str.c_str());
+          }
+        } break;
+        case 1: {
+          if (arg_count != 2) {
+            throw invalid_argument("Fasttrack needs 2 settings");
+          } else {
+            bool base = false, win = false;
+            string temp;
+            ss >> temp;
+            string argument = temp.substr(0, temp.find("="));
+            string value = temp.substr(temp.find("=") + 1, temp.size());
+            if (argument == "baselines") {
+              base = true;
+              msg.arg2 = htonl(stoi(value));
+            } else {
+              win = true;
+              ss >> msg.arg3;
+              msg.arg3 = htonl(stoi(value));
+            }
+            ss >> temp;
+            argument = temp.substr(0, temp.find("="));
+            value = temp.substr(temp.find("=") + 1, temp.size());
+            if (argument == "baselines") {
+              base = true;
+              msg.arg2 = htonl(stoi(value));
+            } else {
+              win = true;
+              ss >> msg.arg3;
+              msg.arg3 = htonl(stoi(value));
+            }
+            if (!(base && win)) {
+              cout << msg.arg2 << "\t" << msg.arg3 << endl;
+              throw invalid_argument("Fasttrack needs two arguments");
+            }
+          }
+        } break;
+        case 2: {
+          if (arg_count != 1) {
+            throw invalid_argument("Boomer needs 1 settings");
+          } else {
+            string temp;
+            getline(ss, temp, '=');
+            ss >> msg.arg2;
+            msg.arg2 = htonl(msg.arg2);
+          }
+        } break;
+        default:
+          throw logic_error("Invalid gamenumber entered");
+        }
+      } else {
+        throw invalid_argument("Unable to parse: \'" + str +
+                               "\', the correct sytnax is \'!battle <gamemode> "
+                               "<gameoptions> <players>{ 1, 7}\'");
+      }
     } else if (regex_match(command, CMD_QUICK)) {
-      msg.message_type = QUICKPLAY;
+      msg.message_type = (tmessage_t)htonl((int32_t)QUICKPLAY);
+      if (regex_match(str, CMD_QUICK_FULL)) {
+        ss >> msg.arg1;
+        msg.arg1 = htonl(msg.arg1);
+      } else {
+        throw invalid_argument("Unable to parse: \'" + str +
+                               "\', the correct sytnax is \'!quick [1-7]");
+      }
     } else if (regex_match(command, CMD_CHILL)) {
-      msg.message_type = CHILL;
+      msg.message_type = (tmessage_t)htonl((int32_t)CHILL);
+      if (!regex_match(str, CMD_QUICK)) {
+        throw invalid_argument("Chill does not expect arguments");
+      }
     } else if (regex_match(command, CMD_GO)) {
-      msg.message_type = GO;
+      msg.message_type = (tmessage_t)htonl((int32_t)GO);
+      if (regex_match(str, CMD_GO_FULL)) {
+        ss >> msg.arg1;
+        msg.arg1 = htonl(msg.arg1);
+      } else {
+        throw invalid_argument("GO needs a number");
+      }
     } else {
       throw invalid_argument("Unable to parse command");
     }
     return msg;
   } else {
-    tmessage msg;
     msg.message_type = CHAT;
     strcpy(msg.buffer, str.c_str());
     return msg;
--- game-client/client.hh
+++ game-client/client.hh
@@ -8,6 +8,23 @@
 #include <vector>
 using namespace std;
 
+#define CMD_LEADERBOARD regex("!leaderboard")
+#define CMD_LEADERBOARD_FULL regex("!leaderboard [0-3]")
+#define CMD_PLAYERS regex("!players")
+#define CMD_PLAYERSTATS regex("!playerstats")
+#define CMD_PLAYERSTATS_FULL regex("!playerstats \\w{1,11}")
+#define CMD_NICKNAME regex("!nickname")
+#define CMD_NICKNAME_FULL regex("!nickname \\w{1,11}")
+#define CMD_BATTLE regex("!battle")
+#define CMD_BATTLE_FULL                                                        \
+  regex("!battle [0-2] ((baselines|time|winlines)=\\d* ){0,2}((\\w{1,11} "     \
+        "){1,7})?(\\w{1,11} *)")
+#define CMD_QUICK regex("!quick")
+#define CMD_QUICK_FULL regex("!quick [1-7]")
+#define CMD_CHILL regex("!chill")
+#define CMD_GO regex("!go")
+#define CMD_GO_FULL regex("!go [:d:]*")
+
 static vector<string> chat_messages;
 static mutex message_lock;
 
